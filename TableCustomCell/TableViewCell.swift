6a43eb69ffb946f9f2cd565beebcb5a3acfa93e3
shemona-ios <yogesh@Yogeshs-MacBook-Pro.local>
2016-08-23 11:43:05 -0700

    Changes

---
+++ TableCustomCell/TableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  TableCustomCell
+//
+//  Created by Shemona on 8/23/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbl_Name: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TableCustomCell/ViewController.swift
+++ TableCustomCell/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  TableCustomCell
 //
-//  Created by Yogesh on 8/23/16.
+//  Created by Shemona on 8/23/16.
 //  Copyright © 2016 test. All rights reserved.
 //
 
@@ -10,10 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tblView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 6
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath:indexPath) as! TableViewCell
+        cell.lbl_Name.text = indexPath.row.description
+        return cell
+    }
+    
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
